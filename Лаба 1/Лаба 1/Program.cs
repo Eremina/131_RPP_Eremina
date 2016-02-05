3ba572ca18841d8f5cf1e2ebb0e730728e260c97
elmira <elmira0112@mail.ru>
2016-02-05 15:02:56 +0300

    1 Лабораторная

--- Лаба 1/Лаба 1/Program.cs
+++ Лаба 1/Лаба 1/Program.cs
@@ -10,6 +10,70 @@ namespace Лаба_1
     {
         static void Main(string[] args)
         {
+            int x;
+            int y;
+            int z;
+            int t;
+            x = 5;
+            y = 2;
+            z = x * y;
+            Console.WriteLine("Заданы значения x=5, y=2. При умножении этих значений мы получим переменную z");
+            Console.WriteLine("z={0}", z);
+            if (z > 0)
+            {
+                Console.WriteLine("Число z положительное");
+            }
+            else
+            {
+                Console.WriteLine("Число z отрицательное");
+            }
+            if (z % 2 == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Нечетное");
+            }
+            if (z % 5 == 0)
+            {
+                Console.WriteLine("Число делиться на 5 без остатка");
+            }
+            else
+            {
+                Console.WriteLine("Число не делиться на 5 без остатка");
+            }
+            if (z % 10 == 0)
+            {
+                Console.WriteLine("Число делиться на 10 без остатка");
+            }
+            else
+            {
+                Console.WriteLine("Число не делиться на 10 без остатка");
+            }
+            if (z % 20 == 0)
+            {
+                Console.WriteLine("Число делиться на 20 без остатка");
+            }
+            else
+            {
+                Console.WriteLine("Число не делиться на 20 без остатка");
+            }
+            Console.WriteLine("Отнимем от z единицу");
+            t = z - 1;
+            Console.WriteLine("z={0}", t);
+        Start:
+            t--;
+        if (t > 0)
+        {
+            Console.WriteLine("z={0}", t);
+            goto Start;
+        }
+        else
+        {
+            Console.WriteLine("z={0}", t);
+        }
+            Console.ReadKey();
         }
     }
 }
